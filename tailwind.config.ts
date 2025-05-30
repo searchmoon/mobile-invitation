389ebf4b34007c0058dff0e6cc1027494ae74a8f
jeongeun moon <moon901212@gmail.com>
2025-05-30 22:51:12 +0900

    tailwind, shadcn 설치 및 셋팅

---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class", // 다크모드 대응하고 싶다면
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+};
+export default config;
