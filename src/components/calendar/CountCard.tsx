09ce74da501f861b8db37650ebb2a8802c028428
jeongeun moon <moon901212@gmail.com>
2025-06-16 00:03:47 +0900

    countcard 컴포넌트로 분리

---
+++ src/components/calendar/CountCard.tsx
@@ -0,0 +1,12 @@
+function CountCard({ countNum, title }: { countNum: number; title: string }) {
+  return (
+    <div className="py-4 rounded-lg shadow-lg">
+      <div className="flex flex-col items-center font-extralight gap-y-2">
+        <div className="text-2xl text-gray-900">{countNum}</div>
+        <div className="text-gray-400 text-xs">{title}</div>
+      </div>
+    </div>
+  );
+}
+
+export default CountCard;
